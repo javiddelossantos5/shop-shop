842d46fa9d3d7fcf847b53a12c4370e6915f5553
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-06-20 14:21:19 -0600

    added offline functionality using local storage to keep items in the cart to remain after refreshing the page

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -3,6 +3,7 @@ import Cart from '../Cart';
 
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 const CartItem = ({ item }) => {
     const [, dispatch] = useStoreContext();
@@ -12,6 +13,7 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
         });
+        idbPromise('cart', 'delete', { ...item });
     };
 
     const onChange = e => {
@@ -22,12 +24,16 @@ const CartItem = ({ item }) => {
                 type: REMOVE_FROM_CART,
                 _id: item._id
             });
+
+            idbPromise('cart', 'delete', { ...item });
         } else {
             dispatch({
                 type: UPDATE_CART_QUANTITY,
                 _id: item._id,
                 purchaseQuantity: parseInt(value)
             });
+
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
         }
     };
 
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -7,6 +7,7 @@ import spinner from '../assets/spinner.gif'
 
 import { useStoreContext } from '../utils/GlobalState';
 import Cart from '../components/Cart';
+import { idbPromise } from '../utils/helpers';
 
 import {
   REMOVE_FROM_CART,
@@ -25,12 +26,19 @@ function Detail() {
         type: UPDATE_CART_QUANTITY,
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      })
+      });
+    // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+    idbPromise('cart', 'put', {
+      ...itemInCart,
+      purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+    });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 }
       });
+      // if product isn't in the cart yet, add it to the current shopping in IndexedDB
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
@@ -39,6 +47,9 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+
+    // upon removal from cart, delete the item from indexedDB using the `currentProduct._id` to locate what to remove
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
 
@@ -53,15 +64,30 @@ function Detail() {
   const { products, cart } = state;
 
   useEffect(() => {
+    // already in global store
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
+      // retrieve from server
     } else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+      // get cace from idb
+    } else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
     }
-  }, [products, data, dispatch, id]);
+
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
